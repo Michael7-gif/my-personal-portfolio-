ad87a55f8d73b83c1c1f91a80d978a613b6cc3c5
Michael7-gif <mikelchuks41@gmail.com>
2025-03-24 13:45:28 +0100

    Add files via upload

---
+++ class.js
@@ -0,0 +1,85 @@
+// alert("welcome to my page");
+// var myname = 'michael'
+// document.write(myname);
+// console.log(myname);
+// var myage ='15';
+// document.write(myage);
+
+// myage !='15';
+// document.write(myage);
+
+// var myage='18'
+// if (myage > 22) {
+//     document.write("correct");
+//     console.log("correct")
+// }
+// else{
+//     document.write("incorrect");
+
+// }
+
+// var myage='20'
+// if(myage > 19 && myage == 20){
+
+// document.write("correct")
+
+// }
+// else{
+//     document.write("invaild");
+
+// }
+
+// var myage='20'
+// if(myage > 20 || myage == 20){
+
+// document.write("correct")
+
+// }
+// else{
+//     document.write("invaild");
+// }
+
+// var myage='20'
+// if(myage < 20){
+//  document.write("correct");
+//      }
+//     else if (myage > 30) {
+//         document.write("invaild");
+    
+//     }
+//     else if (myage >= 30) {
+//         document.write("correct");
+    
+//     }
+//     else{
+//         document.write("none is correct");
+    
+//     }
+// while loop
+// var text=" " ;
+// var i=5;
+// while (i < 10){
+//     text += "<br> the number is " +i;
+//     i++;
+//     document.write(text);
+// } 
+// var 
+// var number = 4;
+// if(number < 2){
+//     document.write("correct");
+// }else if(number<3){
+//     document.write("incorrect");
+// }else if(number<=4){
+//     document.write("incorrect");
+// }else{
+//     document.write("correct");
+// }
+
+
+
+ var age = 5;
+ while (age < 10){
+    console.log("your age is less than 10");
+age++;
+ }
+ document.write("your are over 10");
